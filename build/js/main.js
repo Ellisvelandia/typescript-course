2ef89be9aa8a8bb54805fb8ea58a72708ec527d8
Ellisvelandia <eyis619l@gmail.com>
2023-06-16 22:16:54 -0500

    typescript

---
+++ build/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+let userName = "cris";
+console.log(userName);
+let meaningOflife;
+let isLoading;
+let album;
+let a = 12;
+let b = "6";
+let c = 2;
+meaningOflife = 42;
+isLoading = true;
+album = true;
+console.log(c * b);
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
+let stringArr = ["One", "Hey", "Ellis"];
+let guitars = ["Strat", "Les Paul", 5150];
+let mixedData = ["EVH", 1984, true];
+stringArr[0] = "Cris";
+stringArr.push("Crisanto");
+let test = [];
+let bands = [];
+bands.push("Blink 182");
+//Tuple
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+let userName: string = "cris";
+console.log(userName);
+let meaningOflife: number;
+let isLoading: boolean;
+let album: any;
+
+let a: number =  12;
+let b: any = "6";
+let c: number = 2;
+
+meaningOflife = 42;
+isLoading = true;
+album = true;
+
+console.log(c * b);
+
+const sum = (a:number, b:string) => {
+  return a + b
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re  = /\w+/g
+
+let stringArr = ["One", "Hey", "Ellis"];
+
+let guitars = ["Strat", "Les Paul", 5150];
+
+let mixedData = ["EVH", 1984, true];
+
+stringArr[0] = "Cris";
+stringArr.push("Crisanto");
+
+let test = [];
+let bands: string[] = [];
+bands.push("Blink 182");
+
+//Tuple
+
+
